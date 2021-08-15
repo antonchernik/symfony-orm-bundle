99987328a7e53b21a0a41ba4313ecb8838b9f2ab
Anton <anton.chernik@gmail.com>
2021-08-15 21:05:02 +0300

    OrmFixtureTrait path to fixture improvement

--- tests/Trait/OrmFixtureTrait.php
+++ tests/Trait/OrmFixtureTrait.php
@@ -11,8 +11,8 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 trait OrmFixtureTrait
 {
     protected ?EntityManager $entityManager;
-
     protected ?LoaderInterface $loader = null;
+    protected string $fixturePath;
 
     public function getEntityManager(): ?EntityManager
     {
@@ -36,14 +36,21 @@ trait OrmFixtureTrait
 
     public function load(array $fixtures): void
     {
-        $path = __DIR__.'/Resources/fixtures/%s';
-        $this->loader->load(array_map(function($item) use ($path) { return sprintf($path, $item);}, $fixtures));
+        $this->loader->load(array_map(function($item) { return sprintf($this->fixturePath, $item);}, $fixtures));
     }
 
     public function setUpORM(ContainerInterface $container): void
     {
         $this->loader = $container->get('fidry_alice_data_fixtures.loader.doctrine');
         $this->entityManager = $container->get('doctrine')->getManager();
+        $path = [
+            $container->get('kernel')->getProjectDir(),
+            'tests',
+            'Resources',
+            'Fixtures',
+            '%s'
+        ];
+        $this->fixturePath = implode(DIRECTORY_SEPARATOR , $path);
     }
 
     public function tearDownORM(): void
